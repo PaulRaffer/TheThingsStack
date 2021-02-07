83033a245323f53bfdb189017822614f68b2c3e7
Paul Raffer <paul.raffer@protonmail.com>
2021-02-07 21:17:43 +0100

    'amd64' -> ''

--- Installing_The_Things_Stack/install.sh
+++ Installing_The_Things_Stack/install.sh
@@ -22,7 +22,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 
 sudo add-apt-repository \
-	"deb [arch=amd64] https://download.docker.com/linux/debian \
+	"deb [arch=$ARCH] https://download.docker.com/linux/debian \
 	$(lsb_release -cs) \
 	stable"
 
